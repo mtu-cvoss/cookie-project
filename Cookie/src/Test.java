1126d290a12f5d96f061e65dee70f438d3aa3cc7
sjshilts <shilts.sj@gmail.com>
2019-02-19 19:53:03 -0500

    Deleted Calvin and cleared out the Test class

--- Cookie/src/Test.java
+++ Cookie/src/Test.java
@@ -3,15 +3,6 @@ public class Test {
 	
 	public static void main (String [] args)
 	{
-		System.out.println("Hello World!");
-
-		System.out.println("Calvin very smelly");
-		System.out.println("have you guys heard of twitch prime?");
-		System.out.println("Help");
-		System.out.println("Help meeeeeeeeeeeeee");
-		System.out.println("Help me");
-		
-		System.out.println("Uh oh fhghetio");
 
 	}
 
