7d01fa3dbc770ea401233be7fc8cbca28af72324
sjshilts <shilts.sj@gmail.com>
2019-02-23 13:55:03 -0500

    Added a userInterface package with .xml, Controller.java and Main.java to use scene builder in eclipse.

---
+++ Cookie/src/userInterface/Controller.java
@@ -0,0 +1,12 @@
+package userInterface;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void pressButton(ActionEvent click){
+        System.out.println("Click");
+    }
+
+
+}
