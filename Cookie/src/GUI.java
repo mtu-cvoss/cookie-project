1cc5d654df03b02e6392b021eeb480b2842f7f26
jkdeyonk <jkdeyonk@mtu.edu>
2019-02-22 19:58:02 -0500

    uhhhhh

---
+++ Cookie/src/GUI.java
@@ -0,0 +1,11 @@
+import javafx.application.*;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class GUI {
+	
+	public static void main(String args[]) {
+	
+}
