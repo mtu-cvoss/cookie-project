d47ecd41661585876274c90090509a959c548416
sjshilts <shilts.sj@gmail.com>
2019-02-23 19:14:30 -0500

    Testing javafx

---
+++ Cookie/src/controls/GetData.java
@@ -0,0 +1,20 @@
+package controls;
+import javafx.event.ActionEvent;
+import javafx.scene.chart.PieChart;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart.Data;
+import inflow.*;
+
+public class GetData {
+	
+	private PieChart inFlow;
+	
+	public PieChart getPieChart(InFlow in) {
+		
+		return inFlow;
+		
+	}
+
+}
